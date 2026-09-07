4ec506c79e4a1f60dab905e4241f38ec837e8968
jiashuoz <jszjosh@gmail.com>
2026-09-06 17:37:24 -0700

    chore: prepare npm CLI beta 0.0.7

---
+++ npm/release.js
@@ -0,0 +1,24 @@
+// Immutable CLI assets built from v0.0.7 (500a24368a4cbcd5f69a8996154d0ad8c6e8745a).
+export const version = '0.0.7';
+export const assets = {
+  "darwin_x64": [
+    "amd64",
+    "be164e21b39cd39b50fc6a66c77e7593624e58cb4d29d9944c2ac70ed48524b4",
+    "3f70013679b7f56609002d8316224feb86291368ba9cdc706d133f4596237a59"
+  ],
+  "darwin_arm64": [
+    "arm64",
+    "fc1b9dafd6bc0432babd26fe28e532d3d97eac7da3e3e4da76fa0bf00c69534e",
+    "bfc4d28bede99e1ba3be72d315a1843d0535d4a8c0bdda0156fc404cd39d6b14"
+  ],
+  "linux_x64": [
+    "amd64",
+    "fcb2bfc529d9ee6b13a841d1d2ce4a51a1e68ae57bd91b91a7c60e157ddda3b4",
+    "e7e9db7bd99fb67b4b72979f84a23724016abeb98a7a57de2b8a326058fa1e1b"
+  ],
+  "linux_arm64": [
+    "arm64",
+    "6202ed5cd082f76b913fbaab25455bc7518d1f2a136d246986d116900b022703",
+    "b17877461afd9785325a73c7fd4d494f2356cb4a3ce9c01c3774ef3d3dbd22a9"
+  ]
+};
